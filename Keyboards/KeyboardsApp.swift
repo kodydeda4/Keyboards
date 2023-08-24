27f2e1e44239f95fcdebb35f4335131c19837e83
Kody Deda <kodydeda4@gmail.com>
2023-08-24 17:39:03 -0400

    Initial Commit

---
+++ Keyboards/KeyboardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardsApp.swift
+//  Keyboards
+//
+//  Created by Kody Deda on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
